18afc21d74a067ca3b2472f00848f99c281a8f1a
Asadbek <149485551+AsadbekXayitov@users.noreply.github.com>
2023-11-18 15:36:46 +0500

    yangi

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 let burger = document.querySelector(".burger");
 let links = document.querySelector(".links");
 let linksLi = document.querySelectorAll(".links li");
-let container = document.querySelector(".container")
+let container = document.querySelector(".container");
+let body = document.querySelectorAll("div")
 
 burger.addEventListener("click", () =>{
     links.classList.toggle("links-active");
@@ -9,9 +10,15 @@ burger.addEventListener("click", () =>{
     linksLi.forEach((link, index) => {
         if (link.style.animation) {
             link.style.animation = "";
+            body.forEach(element => {
+                element.style.filter = "brightness(100%)";
+            });
         } else {
             link.style.animation = `harakat 0.5s ease forwards ${index / 7 + 0.5}s`
+            body.forEach(element => {
+                element.style.filter = "brightness(50%)";
+            });
         }
     });
-    burger.classList.toggle("toggle")
+    burger.classList.toggle("toggle");
 });
